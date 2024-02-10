c62b4c419aded210800cfe3b946633fd17bd8f19
Ahmad Reza Momeni <159524976+ARMOMENI1@users.noreply.github.com>
2024-02-10 16:04:50 +0330

    Add files via upload

---
+++ EX1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    cout << "* * * *" << '\n'
+         << "* * *" << '\n'
+         << "* *" << '\n'
+         << "*" << '\n';
+    // It will be effective in the .exe file.
+    Sleep(5000);
+    // It will be effective in the .exe file.
+    return 0;
+}
---
+++ EX2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    string LetterName = "Ahmad Reza Momeni";
+    string ASCIIName = "065 104 109 097 100 032 082 101 122 097 032 077 111 109 101 110 105";
+
+    cout << "\n \"" << LetterName << "\""
+         << "\n \n" << ASCIIName;
+    // It will be effective in the .exe file.
+    Sleep(5000);
+    // It will be effective in the .exe file.
+    return 0;
+}
